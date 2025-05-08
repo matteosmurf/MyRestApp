c9a3efcfcd5730dedbab711547730812df9ed26e
matteo <matteo.antonelli06@gmail.com>
2025-05-08 11:39:47 +0200

    MenuActivity, WebViewActivity, inizio implementazione validazione login

--- app/src/main/java/com/example/myrestapp/MenuActivity.kt
+++ app/src/main/java/com/example/myrestapp/MenuActivity.kt
@@ -1,35 +1,22 @@
 package com.example.myrestapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val username = findViewById<EditText>(R.id.editText_username)
-        val password = findViewById<EditText>(R.id.editText_password)
-        val btnLogin = findViewById<Button>(R.id.button_login)
 
-        val usernameString = username.text.toString()
-        val passwordString = password.text.toString()
-
-        btnLogin.setOnClickListener{
-            if(usernameString.isEmpty() || passwordString.isEmpty()){
-
-            }
-        }
     }
 }

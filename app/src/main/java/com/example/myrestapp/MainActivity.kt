e57cb0f30506222ff0b1c5ae3827e71db9fcbe52
matteosmurf <matteo.antonelli06@gmail.com>
2025-05-14 23:33:46 +0200

    form validation (non funziona niente)

--- app/src/main/java/com/example/myrestapp/MainActivity.kt
+++ app/src/main/java/com/example/myrestapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myrestapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -28,7 +30,12 @@ class MainActivity : AppCompatActivity() {
 
         btnLogin.setOnClickListener{
             if(usernameString.isEmpty() || passwordString.isEmpty()){
-
+                Toast.makeText(this, "Inserisci username e password", Toast.LENGTH_SHORT).show()
+            } else if (passwordString.length < 8){
+                Toast.makeText(this, "La password deve essere almeno 8 caratteri", Toast.LENGTH_SHORT).show()
+            } else {
+                val myIntent = Intent(this, MenuActivity::class.java)
+                startActivity(myIntent)
             }
         }
     }
